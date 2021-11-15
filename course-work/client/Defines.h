dffe88c874798e10ee562f40cb7359bcbc3a9375
Яков Вишневский <47566864+IakovWish@users.noreply.github.com>
2021-11-15 13:11:35 +0300

    Delete course-work/client directory

--- course-work/client/Defines.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-const int FIELD_X = 0;
-const int FIELD_Y = 0;
-const int FIELD_WIDTH = 500;
-const int FIELD_HEIGHT = 500;
--- course-work/client/Utility.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <QPoint>
-#include <QDebug>
-#include "Defines.h"
-
-QPoint getFieldCoord(const QPoint& pos);
